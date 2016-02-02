aaead74e4b664b93419117b9ef9eb72635ff995f
tarao <tarao.gnn@gmail.com>
2016-02-02 19:50:16 +0900

    Append some lines at the end of HTML body.

--- apiconsole.go
+++ apiconsole.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"log"
@@ -23,6 +24,11 @@ func main() {
 		"/devel/console",
 		"A mount point that the console is served.  Note that the static files related to the console are also served under this path.",
 	)
+	extra := flag.String(
+		"append",
+		"",
+		"Append lines at the end of the body of the console page.",
+	)
 	port := flag.Int(
 		"port",
 		8000,
@@ -39,10 +45,11 @@ func main() {
 	upstreamHandler := httputil.NewSingleHostReverseProxy(upstreamURL)
 
 	go func() {
-		log.Printf("Console: http://localhost:%d%s", *port, *mount)
+		log.Printf("Console: http://localhost:%d%s/", *port, *mount)
 	}()
 
 	panic(http.ListenAndServe(fmt.Sprintf(":%d", *port), &handler{
+		extra:           *extra,
 		staticPath:      *mount,
 		staticHandler:   http.StripPrefix(*mount, staticHandler),
 		upstreamHandler: upstreamHandler,
@@ -50,6 +57,7 @@ func main() {
 }
 
 type handler struct {
+	extra           string
 	staticPath      string
 	staticHandler   http.Handler
 	upstreamHandler http.Handler
@@ -59,6 +67,7 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	method := req.Method
 	path := req.URL.Path
 	res := &responseWithStatus{ResponseWriter: w}
+
 	defer func() {
 		log.Printf(
 			"method:%s\tpath:%s\tstatus:%d",
@@ -68,12 +77,43 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		)
 	}()
 
-	if strings.HasPrefix(req.URL.Path, h.staticPath) {
+	if !strings.HasPrefix(req.URL.Path, h.staticPath) {
+		h.upstreamHandler.ServeHTTP(res, req)
+		return
+	}
+
+	staticPath := strings.TrimPrefix(req.URL.Path, h.staticPath)
+
+	if staticPath == "" {
+		http.Redirect(res, req, h.staticPath+"/", http.StatusMovedPermanently)
+	}
+
+	if !(staticPath == "/" || staticPath == "/index.html") {
 		h.staticHandler.ServeHTTP(res, req)
 		return
 	}
 
-	h.upstreamHandler.ServeHTTP(res, req)
+	box, err := rice.FindBox("static")
+	if err != nil {
+		res.WriteHeader(404)
+		return
+	}
+
+	buf, err := box.Bytes("index.html")
+	if err != nil {
+		http.NotFound(res, req)
+		return
+	}
+
+	res.WriteHeader(200)
+	for _, line := range bytes.Split(buf, []byte{0xa}) {
+		if strings.Index(string(line), "</body>") >= 0 {
+			res.Write([]byte(h.extra))
+			res.Write([]byte{0xa})
+		}
+		res.Write(line)
+		res.Write([]byte{0xa})
+	}
 }
 
 type responseWithStatus struct {
